3bd672bddcfa7d06bcb95019726b0355c9b80dfb
tktktakatuska <blue.mountain.syota@icloud.com>
2023-03-05 16:17:14 +0900

    react 4-6

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,29 +1,12 @@
-import { ColoredMessage } from "./components/ColoredMessage";
+
+import { CssModules } from "./components/Cssmodules";
 
     export const App = () => {
-        return (
-            <>
-            {console.log("こんにちは")}
-                <h1>こんにちは！</h1>
-                <p>お元気ですか</p>
-            </>);
-
-    }
-
-    export const Click = () => {
         //ボタンを押した時に実行する関数を定義
-        const onClickButton = () => {
-            alert("alertの表示確認");
-        };
-
         return(
             <>
-            <h1> こんにちは！</h1>
-            {/* コンポーネントへメッセージを送信 */}
-            <ColoredMessage color = "red"> "お元気ですか" </ColoredMessage>
-            <ColoredMessage color = "blue" message=  "元気です!" />
-
-            <button onClick = {onClickButton}>ボタン</button>
+            
+            <CssModules />
             </>
         );
     };
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,5 @@
 import ReactDom from "react-dom";
 import {App} from "./App";
-import {Click} from "./App";
 
-ReactDom.render(<App />   , document.getElementById("root"));
-ReactDom.render(<Click /> , document.getElementById("root"));
+
+ReactDom.render(<App /> , document.getElementById("root"));
